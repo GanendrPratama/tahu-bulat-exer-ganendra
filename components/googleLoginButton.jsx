06091bf5eb1bf22d249d6b0381ce8c1bc3b3cd2c
GanendrPratama <ganendra1@gmail.com>
2025-01-29 01:50:17 +0700

    try fixing google login

--- components/googleLoginButton.jsx
+++ components/googleLoginButton.jsx
@@ -4,6 +4,29 @@ import  signInWithGoogle  from '@/backend/signInWithGoogle';
 
 export default function GoogleLoginButton() {
     return (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <div>
+            <script src="https://accounts.google.com/gsi/client" async></script>
+            <div
+                id="g_id_onload"
+                data-client_id="<client ID>"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="handleSignInWithGoogle"
+                data-nonce=""
+                data-auto_select="true"
+                data-itp_support="true"
+                data-use_fedcm_for_prompt="true"
+            ></div>
+
+            <div
+                class="g_id_signin"
+                data-type="standard"
+                data-shape="pill"
+                data-theme="outline"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left"
+            ></div>
+        </div>
     )
 }
