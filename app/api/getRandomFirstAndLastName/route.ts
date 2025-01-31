2c33327f8708e869623bc16875d497ee3fac95db
GanendrPratama <ganendra1@gmail.com>
2025-01-31 17:05:26 +0700

    fix build

--- app/api/getRandomFirstAndLastName/route.ts
+++ app/api/getRandomFirstAndLastName/route.ts
@@ -1,16 +1,36 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+
+interface RandomUserResponse {
+  results: [{
+    name: {
+      first: string;
+      last: string;
+    }
+  }]
+}
 
 export async function GET() {
-    try {
-      const response = await fetch('https://randomuser.me/api/')
-      const data = await response.json()
-      const user = data.results[0]
-      return NextResponse.json({
-        firstname: user.name.first,
-        lastname: user.name.last,
-      })
-    } catch (error) {
-      console.error('Error fetching random user:', error)
-      return null
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: 'Failed to fetch random user' },
+        { status: 500 }
+      );
     }
+
+    const data: RandomUserResponse = await response.json();
+    const user = data.results[0];
+
+    return NextResponse.json({
+      firstname: user.name.first,
+      lastname: user.name.last,
+    });
+  } catch (error) {
+    console.error('Error fetching random user:', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
   }
+}
